b6312110a244888a7807a99c2de95068b269e6de
dimi7 <dimi7rof@gmail.com>
2026-04-10 13:41:12 +0300

    fix: add dependencyResolutionManagement

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,5 +5,14 @@ pluginManagement {
         mavenCentral()
     }
 }
+
+dependencyResolutionManagement {
+    repositoriesMode.set(org.gradle.api.artifacts.dsl.RepositoryHandler.RepositoryMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "CarAppAndroid"
 include(":app")
